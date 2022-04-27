4fc67a94b7dae481e35b55602d8ff0ef5917b56b
SFelbeck <stefanfelbeck@gmail.com>
2022-04-26 22:31:36 -0700

    ignore tests and index.js comments

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+// links inquirer, fs and all relevant employee classes and subclasses
 const inquirer = require('inquirer');
 const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
 const fs = require('fs');
+// links path and the generate html file
 const path = require('path');
 const generateHTML = require("./util/generateHtml.js");
-
+//directs the path to create a file named index.html and send it to the dist folder
 const sendFile = path.resolve(__dirname, "dist");
 const fileName = path.join(sendFile, "index.html");
+//creates an array to hold all the employee objects
 const team = [];
-
+//initial function that displays inquirer prompts that allow the user to select what kind of employee they want to add to the array and then calls the appropriate function
 function addEmployees() {
     inquirer.prompt([
         {
@@ -42,6 +45,7 @@ function addEmployees() {
         }
     })
 }
+// displays a series of inquirer prompts and uses the responses to generate a manager employee object and pushes it to the team array before setting up the addEmployees function for the next employee
     function addManager() {
         inquirer
             .prompt([
@@ -73,7 +77,7 @@ function addEmployees() {
                 addEmployees();
             })
     }
-
+// displays a series of inquirer prompts and uses the responses to generate a engineer employee object and pushes it to the team array before setting up the addEmployees function for the next employee
     function addEngineer() {
         inquirer
             .prompt([
@@ -98,14 +102,14 @@ function addEmployees() {
                     message: 'What is the GitHub username of your engineer?'
                 }
             ]
-            ).then((name) => {
-                console.log(name);
-                const newEngineer = new Engineer(name.name, name.id, name.email, name.github);
+            ).then((response) => {
+                console.log(response);
+                const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
                 team.push(newEngineer);
                 addEmployees();
             })
     }
-
+// displays a series of inquirer prompts and uses the responses to generate a intern employee object and pushes it to the team array before setting up the addEmployees function for the next employee
     function addIntern() {
         inquirer
             .prompt([
@@ -130,17 +134,18 @@ function addEmployees() {
                     message: 'What school does your intern go to?'
                 }
             ]
-            ).then((name) => {
-                console.log(name);
-                const newIntern = new Intern(name.name, name.id, name.email, name.school);
+            ).then((response) => {
+                console.log(response);
+                const newIntern = new Intern(response.name, response.id, response.email, response.school);
                 team.push(newIntern);
                 addEmployees();
             })
     }
-
+// writes an html file using the generateHTML template or throws an error if something has gone horribly awry
     function finalize(){
         fs.writeFileSync(fileName, generateHTML(team), "utf-8", function(err){
             if(err)throw err
         })
     }
+    //initial call for the addEmployees function waaaay at the bottom because it felt the most secure here, I dont make the rules
     addEmployees();
